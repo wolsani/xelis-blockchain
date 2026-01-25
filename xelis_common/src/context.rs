1da46b515d72ff003f1bf1c304942a1c45845af3
Slixe <slixeprivate@gmail.com>
2026-01-25 22:04:57 +0100

    common: context comments

--- xelis_common/src/context.rs
+++ xelis_common/src/context.rs
@@ -35,37 +35,46 @@ impl BuildHasher for NoOpBuildHasher {
     }
 }
 
+/// A static Context to store/retrieve data by type
 pub struct Context {
     values: HashMap<TypeId, Box<dyn Any + Send + Sync>, BuildHasherDefault<NoOpHasher>>,
 }
 
 impl Context {
+    /// Create a new empty Context
+    #[inline]
     pub fn new() -> Self {
         Self {
             values: HashMap::default()
         }
     }
 
+    /// Store a value in the Context
     pub fn store<T: Send + Sync + 'static>(&mut self, data: T) {
         self.values.insert(TypeId::of::<T>(), Box::new(data));
     }
 
+    /// Remove a value from the Context
     pub fn remove<T: 'static>(&mut self) {
         self.values.remove(&TypeId::of::<T>());
     }
 
+    /// Check if a value of type T exists in the Context
     pub fn has<T: 'static>(&self) -> bool {
         self.values.contains_key(&TypeId::of::<T>())
     }
 
+    /// Get an optional reference to a value of type T from the Context
     pub fn get_optional<T: 'static>(&self) -> Option<&T> {
         self.values.get(&TypeId::of::<T>()).and_then(|b| b.downcast_ref())
     }
 
+    /// Get a reference to a value of type T from the Context or return an error if not found
     pub fn get<T: 'static>(&self) -> Result<&T> {
         self.get_optional().context("Requested type not found")
     }
 
+    /// Get a copy of a value of type T from the Context or return an error if not found
     pub fn get_copy<T: 'static + Copy>(&self) -> Result<T> {
         self.get().copied()
     }
